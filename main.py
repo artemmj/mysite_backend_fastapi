d2b6f9d627f02c3f98465669440e1bf62e3a15fb
Artem_M <web.ep4@gmail.com>
2025-04-29 15:45:40 +0300

    Итерация с fastapi

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path, Body, Form
 from fastapi.responses import Response, HTMLResponse, JSONResponse,  PlainTextResponse, FileResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return Response(content="<h1>=== Hello FastAPI! main page ===</h1>", media_type="text/html")
+    return FileResponse("public/index.html")
 
-@app.get("/simple", response_class=PlainTextResponse)
-def simple_reponse():
-    return PlainTextResponse(content="=== simple text ===", media_type="text/plain")
 
-@app.get("/html", response_class=HTMLResponse)
-def simple_reponse():
-    return HTMLResponse(content="<h1>=== html text ===</h1>")
-
-@app.get("/file", response_class = FileResponse)
-def about():
-    return FileResponse(path="public/index.html")
-
-@app.get("/json", response_class = JSONResponse)
-def json():
-    json_data = jsonable_encoder({"message": "success json response!"})
-    return JSONResponse(content=json_data)
+@app.post("/postdata")
+def postdata(username = Form(), userage=Form()):
+    return {"name": username, "age": userage}
